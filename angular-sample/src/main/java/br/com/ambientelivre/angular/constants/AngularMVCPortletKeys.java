0946e8aaf2e33c996687655e988842169d059f4a
Marcio Junior Vieira <marcio@ambientelivre.com.br>
2021-02-03 12:55:10 -0300

    Sample AngularJS

---
+++ angular-sample/src/main/java/br/com/ambientelivre/angular/constants/AngularMVCPortletKeys.java
@@ -0,0 +1,10 @@
+package br.com.ambientelivre.angular.constants;
+
+/**
+ * @author marcio
+ */
+public class AngularMVCPortletKeys {
+
+	public static final String AngularMVC = "angularmvc";
+
+}
---
+++ angular-sample/src/main/java/br/com/ambientelivre/angular/portlet/AngularMVCPortlet.java
@@ -0,0 +1,51 @@
+package br.com.ambientelivre.angular.portlet;
+
+import br.com.ambientelivre.angular.constants.AngularMVCPortletKeys;
+
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import java.io.IOException;
+
+import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author marcio
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + AngularMVCPortletKeys.AngularMVC,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class AngularMVCPortlet extends MVCPortlet {
+
+	@Override
+	public void doView(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws IOException, PortletException {
+
+		renderRequest.setAttribute(
+			"mainRequire",
+			_npmResolver.resolveModuleName("angular-sample") + " as main");
+
+		super.doView(renderRequest, renderResponse);
+	}
+
+	@Reference
+	private NPMResolver _npmResolver;
+
+}
