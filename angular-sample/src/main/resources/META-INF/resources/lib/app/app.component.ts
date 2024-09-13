74f4c80166d893a8735c1d4d51236fc8919e67ec
Mariana Batista <mariana-batista99@hotmail.com>
2024-09-13 11:12:45 -0300

    update angular-sample

--- angular-sample/src/main/resources/META-INF/resources/lib/app/app.component.ts
+++ angular-sample/src/main/resources/META-INF/resources/lib/app/app.component.ts
@@ -8,13 +8,38 @@ export class Hero {
 @Component({
 	template: `
 	<h1>{{title}}</h1>
+	<h2>ID: {{hero.id}}</h2>
 	<h2>{{hero.name}} details!</h2>
-	<div><label>id: </label>{{hero.id}}</div>
+	<div>
+		<label>id: </label>
+		<input [(ngModel)]="hero.id" placeholder="id">
+	</div>
 	<div>
 		<label>name: </label>
 		<input [(ngModel)]="hero.name" placeholder="name">
 	</div>
 	`,
+	styles: [`
+    h1 {
+      margin-top: 30px;
+      color: green;
+    }
+    h2 {
+      text-decoration: underline;
+      text-decoration-color: green;
+    }
+    label {
+    	font-size: 18px;
+    }
+    input {
+      margin-top: 15px;
+      border: 1px solid green;
+      border-radius: 5px;
+    }
+    input:focus {
+      border-color: green;
+    }
+  `],
 })
 export class AppComponent {
 	hero: Hero = {
